e288813779493e4d0082ec4fbdd2cf6d2d27f46c
Valsury <valsury@mail.ru>
2024-02-29 12:25:30 +0300

    Create text pannels

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,56 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Main {
     public static void main(String[] args) {
 
+        JTextField smallField = new JTextField("300, 250, 25, 45, 20");
+
+        JTextField numbers = new JTextField("0, 1, 2, 3, 4, 5, 6, 7, 8, 9");
+
+        numbers.addActionListener(new ActionListener() {
+
+
+
+            public void actionPerformed(ActionEvent event) {
+
+                String[] parm = smallField.getText().replaceAll(" ", "").split(",");
+
+                int[] koords = new int[parm.length];
+
+                if (parm.length != parm.length) {
+
+                    //Отображение введенного текста
+
+                }
+
+                JFrame f = new JFrame("Swing Paint Demo");
+
+                JOptionPane.showMessageDialog( null,  "Ошибка в количестве параметров");
+
+            }
+
+        });
         JFrame f = new JFrame("Swing Paint Demo");
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+        JPanel contents = new JPanel(new FlowLayout(FlowLayout.LEFT));
+
+
+        contents.add(numbers);
+
+        contents.add(smallField);
+
+        f.setContentPane(contents);
 
         MyPanel myPanel = new MyPanel();
 
         myPanel.repaint();
 
         myPanel.setSymbols("1, 2, 3");
-
 
         f.add(myPanel);
 
